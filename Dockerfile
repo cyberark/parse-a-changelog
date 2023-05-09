a8714a3b15ca06b71754cd217c06c609b215e955
junior.taeza <junior.taeza@cyberark.com>
2023-05-09 14:16:55 -0400

    Upgrade ruby from 2.6 to 3.1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:2.6
+FROM ruby:3.1
 
 RUN gem install bundler --no-document
 
--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -eux
 docker run \
     -v $PWD:/work \
     -w /work \
-    ruby:2.6 \
+    ruby:3.1 \
         bash -c "gem install bundler --no-document; \
                  bundle install; \
                  bundle exec rspec"
